32006ede8794bf1dac12a05d8d6580ad80dec6b7
Minkyo_Kim <83809702+gmrdns03@users.noreply.github.com>
2021-06-22 23:55:16 +0900

    Delete Python01_07_ifEx03Or_김민교.py

--- Python01_07_ifEx03Or_김민교.py
+++
@@ -1,8 +0,0 @@
-dept01 = "영업부"
-dept02 = "전산부"
-dept03 = "기획부"
-dept04 = "총무부"
-if dept01 == "총무부" or dept02 == "전산부" and dept03 == "총무부" or dept04 == "전산부":
-	print("서울 부서 입니다.")
-else:
-	print("부산 부서 입니다.")
